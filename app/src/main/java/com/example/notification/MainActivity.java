36274874122de10144734a97072b4a0363ea1f4b
Sanjana Sawant <sanjanasawant1505@gmail.com>
2021-07-05 12:41:04 +0530

    Notification

---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView btnNotify;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnNotify = findViewById(R.id.btnNotify);
+
+        btnNotify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotification();
+            }
+        });
+    }
+
+    public void showNotification() {
+        final String NOTIFICATION_CHANNEL_ID = "10001";
+        final String default_notification_channel_id = "default";
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this,
+                default_notification_channel_id)
+                .setSmallIcon(R.drawable.snap)
+                .setContentTitle("Snapchat")
+                .setContentText("Sanjana has send you the snap 15s ago");
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            assert mNotificationManager != null;
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+        assert mNotificationManager != null;
+        mNotificationManager.notify((int) System.currentTimeMillis(), mBuilder.build());
+
+    }
+
+
+}
